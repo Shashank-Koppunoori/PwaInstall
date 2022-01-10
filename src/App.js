94bd7ebe751acf31cd282fc22550a0ff104e9c2a
Shashank-Koppunoori <kopunurushashank@ula.app>
2022-01-10 17:21:03 +0530

    asda

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import InstallPwa from "./InstallPwa";
 function App() {
+
   return (
     <div className="App">
+      <InstallPwa />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/InstallPwa.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const InstallPwa = () => {
+    const [showInstallApp, setShowInstallApp] = React.useState(true);
+  React.useEffect(() => {
+
+    let deferredPrompt;
+    const addBtn = document.querySelector('.install-app-button');
+
+    // Checks if users is using installed pwa
+    if (window.navigator.standalone || window.matchMedia('(display-mode: standalone)').matches) {
+      setShowInstallApp(false);
+    }
+    
+    // In iOS devices(iPhone,iPad,iPod) only Safari supports pwa install
+    // beforeinstallprompt event is not compatable in safari browser, so hide Install button in all ios devices & safari (in mac)
+    if (window.navigator.userAgent.match(/Version\/[0-9.]+.*Safari/) || /(iPhone|iPad|iPod)/.test(window.navigator.userAgent)) {
+      setShowInstallApp(false);
+    }
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('beforeinstallprompt', e => {
+        e.preventDefault();
+        deferredPrompt = e;
+        addBtn.addEventListener('click', () => {
+          deferredPrompt.prompt();
+          deferredPrompt.userChoice.then(choiceResult => {
+            if (choiceResult.outcome === 'accepted') {
+              setShowInstallApp(false);
+            }
+            deferredPrompt = null;
+          });
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      {showInstallApp && (
+        <button className='install-app-button' type='button'>
+          Install
+        </button>
+      )}
+    </>
+)
+}
+
+export default InstallPwa;
